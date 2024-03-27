154b5be3d92d0a8e3460530464cbb731e0b498b4
Adam McKee <adam.be.g84d@gmail.com>
2024-03-27 18:08:28 -0500

    start host runtime with wasi apis

--- runtime/rust/src/main.rs
+++ runtime/rust/src/main.rs
@@ -1,11 +1,10 @@
 use wasmtime::{Config, Engine, Store};
 use wasmtime::component::{Component, Linker};
+use wasmtime_wasi::{WasiCtxBuilder, WasiP1Ctx};
 
 wasmtime::component::bindgen!({
     path: "../../wit",
 });
-
-struct RuntimeState;
 
 fn main() -> wasmtime::Result<()> {
     let input: i32 = std::env::args().last().unwrap().parse()?;
@@ -15,9 +14,12 @@ fn main() -> wasmtime::Result<()> {
     let engine = Engine::new(&config)?;
     let component = Component::from_file(&engine, "../../guest/golang/build/is_odd_or_even.component.wasm")?;
 
-    let mut linker = Linker::new(&engine);
+    let mut linker: Linker<WasiP1Ctx> = Linker::new(&engine);
+    wasmtime_wasi::command::sync::add_to_linker(&mut linker)?;
+    let wasi = WasiCtxBuilder::new().build_p1();
+    let mut store = Store::new(&engine, wasi);
 
-    let mut store = Store::new(&engine, RuntimeState {});
+    let (bindings, _) = Lib::instantiate(&mut store, &component, &linker)?;
 
     if bindings.call_is_odd_or_even(&mut store, input)? {
         println!("{} is even", input)
